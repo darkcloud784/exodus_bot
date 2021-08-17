c2f3fb92f994b93c65c09e768744b29ae6304842
darkcloud784 <mrkrodriguez@gmail.com>
2021-08-17 13:21:55 -0500

    remove bot check

--- commands/recruit.js
+++ commands/recruit.js
@@ -1,5 +1,4 @@
 exports.run = (message, member) => {
-    if (message.author.bot) return;
     //const settings = message.settings = client.getSettings(message.guild);
     const recruitMsg = "Hello {{user}}, We are currently in need of shamans of all specializations as well as caster dps and a couple hunters. Specically we would like to have more warlocks and boomkins. If your interested in joining, please message one of our officers!!";
     const recruit = recruitMsg.replace("{{user}}", member.user.tag);
