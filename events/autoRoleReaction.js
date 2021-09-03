66deba6040cbc758e160abba2b90dac60b2c3714
darkcloud784 <mrkrodriguez@gmail.com>
2021-09-03 09:49:30 -0500

    would help if I copy the correct role id.

--- events/autoRoleReaction.js
+++ events/autoRoleReaction.js
@@ -10,7 +10,7 @@ module.exports = (messageReactionAdd, (reaction, user) => {
     else if (reaction.message.channel.id === '865566305842757632') {
         if (reaction.emoji.name == "bust_in_silhouette") {
             const guildMember = reaction.message.guild.members.get(user.id);
-            const guestRole = reaction.message.roles.get('862521361212637224');
+            const guestRole = reaction.message.roles.get('865656133859540992');
             guildMember.addrole(guestRole);
         }
     }
