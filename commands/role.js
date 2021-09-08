9d84eab25ba230749ce28ec7bcea65d492e428ab
darkcloud784 <mrkrodriguez@gmail.com>
2021-09-08 09:20:31 -0500

    test

--- commands/role.js
+++ commands/role.js
@@ -1,6 +1,7 @@
 exports.run = async (message, args, Discord, client) => {
     //async execute(message, args, Discord, client) {
         const channel = '865566305842757632';
+
         const memberRole = message.guild.roles.cache.find(role => role.name == "Members");
         const guestMember = message.guild.roles.cache.find(role => role.name == "Guest");
 
@@ -15,6 +16,27 @@ exports.run = async (message, args, Discord, client) => {
         + `${guestEmoji} if your a Guest`);
 
         let messageEmbed = await message.channel.send(embed);
+        messageEmbed.react(memberEmoji);
+        messageEmbed.react(guestEmoji);
+
+        client.on('messageReactionAdd', async (reaction, user) => {
+            if (reaction.message.partial) await reaction.message.fetch();
+            if (reaction.partial) await reaction.fetch();
+            if (user.bot) return;
+            if (!reaction.message.guild) return;
+
+            if(reaction.message.channel.id == channel){
+                if (reaction.emoji.name === memberEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.add(memberRole);
+                }
+                if (reaction.emoji.name == guestEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.add(guestMember);
+                }
+            } else {
+                return;
+            }
+
+        });
     }
 
     exports.conf = {
