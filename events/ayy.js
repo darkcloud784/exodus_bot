e2a04c96e7d4ced372e88d2dc9a6d0e29c2965ee
darkcloud784 <mrkrodriguez@gmail.com>
2021-09-25 16:57:57 -0500

    not 100% sure why this isn't working.

--- events/ayy.js
+++ events/ayy.js
@@ -1,17 +1,18 @@
 Discord = require('discord.js');
-  
-module.exports = (client, message, args) => {
-  //ayy
-const embed = new Discord.MessageEmbed()
-.setTitle('ayy')
-.attachFiles('../images/ayy.jpg')
-.setImage('attachment://ayy.jpg');  
-  if (message.content.indexOf('ayy') >= 0 || message.content.indexOf('Ayy') >= 0) {
+
+
+module.exports = (client, message) => {
+  if (message.author.bot) return;
+  const embed = new Discord.MessageEmbed()
+    .setTitle('ayy')
+    .attachFiles('../images/ayy.jpg')
+    .setImage('attachment://ayy.jpg');
+  if (message.content.includes('ayy') || message.content.includes('Ayy')) {
     var lmaos = [
       'ayy lmao! :alien:',
       'remember the ayylmao! :alien: :face_with_cowboy_hat:',
       ('Your new name is ' + "@" + message.author.username + 'ayy Lmao! :alien:'),
-      message.channel.send({ embed })
+      ('Ayy lmao' + message.channel.send({ embed }))
     ];
     var lmao = lmaos[Math.floor(Math.random() * lmaos.length)];
     message.reply(lmao);
